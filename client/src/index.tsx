d74dafd6b8480a2c141219443265c7af93094963
fronix <fronix@fronix.se>
2019-11-09 19:11:26 +0100

    Client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<h1>Hello :)</h1>, document.getElementById('root'));
