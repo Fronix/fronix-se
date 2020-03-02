51d7065cad61a68bcf4d61b0c20837629defbc52
fronix <fronix@fronix.se>
2020-03-02 21:35:11 +0100

    Add console load ascii text

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,30 @@
-import CssBaseline from '@material-ui/core/CssBaseline';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import React, { useMemo } from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 import * as serviceWorker from './serviceWorker';
 import GlobalCss from './themes/globalCss';
-import getTheme from './themes/theme';
 import { StoreProvider } from 'easy-peasy';
 import store from './store';
 import ThemeProvider from './themes/ThemeProvider';
+import packageJson from '../package.json';
+import Axios from 'axios';
 
 const App = () => {
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-  const theme = useMemo(() => getTheme(prefersDarkMode), [prefersDarkMode]);
+  const consoleText = Axios.get(
+    `https://gist.githubusercontent.com/Fronix/ad142871aecd0e3dd499a32d08c53c77/raw/3d78a5409b325ba0a0a6a93055f237a673255853/consoleLoad.txt`
+  );
+
+  useEffect(() => {
+    consoleText.then(res => {
+      console.log(res.data);
+      console.log(
+        `%cVersion: ${packageJson.version}`,
+        'color: #66D9EF; font-size: 24px; font-family: Monospace;'
+      );
+    });
+  }, [consoleText]);
 
   return (
     <StoreProvider store={store}>
