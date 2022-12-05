55e1eabf9aeece1bf67b93e1bdb819729aff115e
Oscar Martin <oscar.martin@tietoevry.com>
2022-12-05 22:24:38 +0100

    Init nextra

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,35 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+import LastUpdated from "./components/Lastupdated";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  logo: <span>fronix.se</span>,
+  docsRepositoryBase: "https://github.com/fronix/fronix-se",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "fronix.se",
   },
-}
+  editLink: {
+    text: "",
+  },
+  feedback: {
+    content: undefined,
+  },
+  search: {
+    component: undefined,
+  },
+  gitTimestamp: LastUpdated,
+  useNextSeoProps() {
+    return {
+      title: "fronix - i do stuff",
+      description: "i watch movies and do some coding",
+      openGraph: {
+        type: "website",
+        locale: "sv_SE",
+        url: "https://fronix.se",
+        site_name: "fronix.se",
+      },
+    };
+  },
+};
 
-export default config
+export default config;
