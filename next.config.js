f98132b538cad14b457379cb2d7c22fc37e53d94
Oscar Martin <oscar.martin@tietoevry.com>
2022-12-05 22:34:31 +0100

    Add dockerfile

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
 
-module.exports = withNextra()
+// @ts-check
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone",
+};
+
+module.exports = withNextra(nextConfig);
